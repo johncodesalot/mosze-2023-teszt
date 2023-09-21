d2bc7c9c3b0223381bd91a2cbc3fee7e86382fea
johncodesalot <nagy.jan95@gmail.com>
2023-09-21 17:31:44 +0200

    Disturbance in the Force

--- mosze-2023-teszt/main.cpp
+++ mosze-2023-teszt/main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //typo
+    std::cout << '1-100 ertekek duplazasa' //syntax
+    for (int i = 0;)                        //unfinished
     {
-        b[i] = i * 2;
+        b[i] = i * 2;                       //wrong init
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"               //unfinished
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;                              //wrong type, init missing
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
